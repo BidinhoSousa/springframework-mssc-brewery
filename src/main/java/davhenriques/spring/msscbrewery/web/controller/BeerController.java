21ab0cdef2be15f63be026861dc00f608ff5fec5
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2022-07-07 10:20:55 +0100

    Added class BeerDto.java

---
+++ src/main/java/davhenriques/spring/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,24 @@
+package davhenriques.spring.msscbrewery.web.controller;
+
+import davhenriques.spring.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Created by DavHenriques on 07/07/2022
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+}
