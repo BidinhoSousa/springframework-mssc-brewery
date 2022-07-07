021c024076294b13f152ddf5d3bf4e0357e4e67f
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2022-07-07 11:19:36 +0100

    Added class BeerController

---
+++ src/main/java/davhenriques/spring/msscbrewery/service/BeerService.java
@@ -0,0 +1,13 @@
+package davhenriques.spring.msscbrewery.service;
+
+import davhenriques.spring.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by DavHenriques on 07/07/2022
+ */
+public interface BeerService {
+
+    BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/davhenriques/spring/msscbrewery/service/BeerServiceImpl.java
@@ -0,0 +1,20 @@
+package davhenriques.spring.msscbrewery.service;
+
+import davhenriques.spring.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by DavHenriques on 07/07/2022
+ */
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
